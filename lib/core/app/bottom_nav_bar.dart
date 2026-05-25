32c3dae0ceee48aa162fcce846ecbb3e21ff735e
Anandu Udayan <anandhuudayan180@gamil.com>
2026-05-25 15:24:39 +0530

    feat(navbar): implement responsive bottom navigation bar with breakpoints

--- lib/core/app/bottom_nav_bar.dart
+++ lib/core/app/bottom_nav_bar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:netflix_clone/core/constants/app_breakpoints.dart';
 
 class BottomNavBar extends StatelessWidget {
   final void Function(int) onTap;
@@ -12,29 +13,53 @@ class BottomNavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      type: BottomNavigationBarType.fixed,
-      currentIndex: currentIndex,
-      onTap: onTap,
-      items: const [
-        BottomNavigationBarItem(
-          icon: Icon(CupertinoIcons.house_fill),
-          label: "Home",
+    final size = MediaQuery.of(context).size;
+    final isMobile = size.width < AppBreakpoints.mobile;
+    final isTablet =
+        size.width > AppBreakpoints.mobile &&
+        size.width < AppBreakpoints.tablet;
+    return SizedBox(
+      height: 90,
+      child: Align(
+        alignment: isMobile ? Alignment.bottomCenter : Alignment.center,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(isMobile ? 0 : 30),
+          child: SizedBox(
+            width: isMobile
+                ? double.infinity
+                : isTablet
+                ? AppBreakpoints.tabletNavBarWidth
+                : AppBreakpoints.desktopNavBarWidth,
+            child: BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              currentIndex: currentIndex,
+              onTap: onTap,
+              items: const [
+                BottomNavigationBarItem(
+                  icon: Icon(CupertinoIcons.house_fill),
+                  label: "Home",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(CupertinoIcons.flame_fill),
+                  label: "New & Hot",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.emoji_emotions),
+                  label: "Fast Laughs",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(CupertinoIcons.search),
+                  label: "Search",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.download),
+                  label: "Downloads",
+                ),
+              ],
+            ),
+          ),
         ),
-        BottomNavigationBarItem(
-          icon: Icon(CupertinoIcons.flame_fill),
-          label: "New & Hot",
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.emoji_emotions),
-          label: "Fast Laughs",
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(CupertinoIcons.search),
-          label: "Search",
-        ),
-        BottomNavigationBarItem(icon: Icon(Icons.download), label: "Downloads"),
-      ],
+      ),
     );
   }
 }
---
+++ lib/core/constants/app_breakpoints.dart
@@ -0,0 +1,6 @@
+class AppBreakpoints {
+  static const double mobile = 600;
+  static const double tablet = 1000;
+  static const double tabletNavBarWidth = 500;
+  static const double desktopNavBarWidth = 600;
+}
