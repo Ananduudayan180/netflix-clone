6eb1691ffa9a7b9925c0c0fcf92e901fda21a6ba
Anandu Udayan <anandhuudayan180@gamil.com>
2026-05-25 18:03:57 +0530

    feat(constants): add AppImageUrl and AppStr classes

---
+++ lib/core/constants/app_image_url.dart
@@ -0,0 +1,4 @@
+class AppImageUrl {
+  static const appLogo = 'assets/images/core/netflix_logo.png';
+  static const netflixAi = 'assets/images/core/netflix_ai.jpeg';
+}
---
+++ lib/core/constants/app_str.dart
@@ -0,0 +1,10 @@
+class AppStr {
+  //dowloads page section
+  static const String downloads = 'Downloads';
+  static const String smartDownloads = 'Smart Downloads';
+  static const String introducingDownloads = 'Introducing Downloads for You';
+  static const String downloadSubtitle =
+      "We'll download apersonalised selection of\n movies and shows for you, so there's\n always something to watch on your\n device.";
+  static const String setUpButton = 'Set Up';
+  static const String seeDowloadsButton = 'See What You Can Download';
+}
